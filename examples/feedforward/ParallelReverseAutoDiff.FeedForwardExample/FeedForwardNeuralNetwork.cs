c8d5b089c6983bac82ce550386e00524af81994f
Scott Seto <scott.seto@mitechnicalsolutions.com>
2023-05-05 17:18:23 -0400

    Add feed forward example.

---
+++ examples/feedforward/ParallelReverseAutoDiff.FeedForwardExample/FeedForwardNeuralNetwork.cs
@@ -0,0 +1,14 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="FeedForwardNeuralNetwork.cs" author="ameritusweb" date="5/5/2023">
+// Copyright (c) 2023 ameritusweb All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+namespace ParallelReverseAutoDiff.FeedForwardExample
+{
+    /// <summary>
+    /// A feed forward neural network.
+    /// </summary>
+    public class FeedForwardNeuralNetwork
+    {
+    }
+}
