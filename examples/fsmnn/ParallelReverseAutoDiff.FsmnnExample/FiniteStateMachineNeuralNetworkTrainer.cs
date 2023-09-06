18ab371a5e6d154ec0b1cc38ddd51748a39adf69
Scott Seto <scott.seto@mitechnicalsolutions.com>
2023-09-06 10:15:34 -0400

    Update trainer.

--- examples/fsmnn/ParallelReverseAutoDiff.FsmnnExample/FiniteStateMachineNeuralNetworkTrainer.cs
+++ examples/fsmnn/ParallelReverseAutoDiff.FsmnnExample/FiniteStateMachineNeuralNetworkTrainer.cs
@@ -17,11 +17,31 @@ namespace ParallelReverseAutoDiff.FsmnnExample
         /// <summary>
         /// Train the network.
         /// </summary>
-        public void Train()
+        /// <returns>The task.</returns>
+        public async Task Train()
         {
             MazeMaker makeMaker = new MazeMaker();
             var maze = makeMaker.CreateMaze(10);
             FiniteStateMachineTraversalNeuralNetwork traversalNetwork = new FiniteStateMachineTraversalNeuralNetwork(maze, 6, maze.NumIndices, maze.AlphabetSize, 10, 2, 2, 0.001d, 4d);
+
+            for (int i = 0; i < 100; ++i)
+            {
+                await this.RunIteration(traversalNetwork);
+            }
+        }
+
+        /// <summary>
+        /// Runs an iteration of training.
+        /// </summary>
+        /// <param name="traversalNetwork">The traversal network.</param>
+        /// <returns>The task.</returns>
+        public async Task RunIteration(FiniteStateMachineTraversalNeuralNetwork traversalNetwork)
+        {
+            await traversalNetwork.Initialize();
+            var gradient = traversalNetwork.Forward();
+            await traversalNetwork.Backward(gradient);
+            traversalNetwork.ApplyGradients();
+            await traversalNetwork.Reset();
         }
     }
 }
