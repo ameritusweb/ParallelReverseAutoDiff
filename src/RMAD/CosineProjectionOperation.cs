28dd84b93199f178129a4e21da00bcdf9f133b01
Scott Seto <scott.seto@outlook.com>
2023-12-24 16:21:39 -0500

    Update to v1.1.31

--- src/RMAD/CosineProjectionOperation.cs
+++ src/RMAD/CosineProjectionOperation.cs
@@ -1,5 +1,5 @@
 ﻿//------------------------------------------------------------------------------
-// <copyright file="CosineProjectionOperation.cs" author="ameritusweb" date="9/4/2023">
+// <copyright file="CosineProjectionOperation.cs" author="ameritusweb" date="12/4/2023">
 // Copyright (c) 2023 ameritusweb All rights reserved.
 // </copyright>
 //------------------------------------------------------------------------------
@@ -52,24 +52,24 @@ namespace ParallelReverseAutoDiff.RMAD
         {
             this.inputMatrix = inputMatrix;
             this.learnedScalings = p;
-            this.Output = new Matrix(inputMatrix.Cols, N + 1);
+            this.Output = new Matrix(inputMatrix.Cols, N);
 
             for (int j = 0; j < inputMatrix.Cols; ++j)
             {
                 double decimalNumber = inputMatrix[0][j];
-                double[] vector = new double[N + 1];
+                double[] vector = new double[N];
 
                 for (int i = 0; i < N; i++)
                 {
-                    // Scale up, extract the digit, and scale down
-                    double scaledNumber = decimalNumber * Math.Pow(10, i);
-                    double digit = (Math.Floor(scaledNumber) % 10) + 1; // adds 1 to prevent division by zero in the backward function
+                    // Calculate the influence of the i-th digit
+                    double influence = this.CalculateDigitInfluence(decimalNumber, i);
 
-                    // Generate the scaled unit vector
-                    double scaledValue = digit * Math.Pow(10, p[0][i]) / 10;
+                    // Generate the scaled unit vector. The scaling is based on learned parameters and the digit's influence
+                    double[] scaledUnitVector = new double[N];
+                    scaledUnitVector[i] = influence * Math.Pow(10, p[0][i]) / 10;
 
-                    // Compute the magnitude of the output vector
-                    vector[i + 1] = digit / 10 * Math.Sqrt(N + 1) / scaledValue;
+                    // Compute the magnitude of the output vector. The magnitude is adjusted based on the digit's influence
+                    vector[i] = (influence / 10) * Math.Sqrt(N) / scaledUnitVector[i];
                 }
 
                 this.Output[j] = vector;
@@ -81,7 +81,7 @@ namespace ParallelReverseAutoDiff.RMAD
         /// <inheritdoc />
         public override BackwardResult Backward(Matrix dOutput)
         {
-            Matrix dInputMatrix = new Matrix(this.inputMatrix.Rows, this.inputMatrix.Cols); // Empty, as this is used at the beginning of the network.
+            Matrix dInputMatrix = new Matrix(this.inputMatrix.Rows, this.inputMatrix.Cols); // Can be left empty
             Matrix dLearnedScalings = new Matrix(this.learnedScalings.Rows, this.learnedScalings.Cols);
 
             for (int j = 0; j < this.inputMatrix.Cols; ++j)
@@ -90,24 +90,38 @@ namespace ParallelReverseAutoDiff.RMAD
 
                 for (int i = 0; i < N; i++)
                 {
-                    double scaledNumber = decimalNumber * Math.Pow(10, i);
-                    double digit = (Math.Floor(scaledNumber) % 10) + 1; // Same as in Forward
+                    // Gradient of the influence function with respect to the decimalNumber
+                    double influence = this.CalculateDigitInfluence(decimalNumber, i);
+                    double influenceGradient = influence * (1 - influence); // derivative of sigmoid
 
-                    double scaledValue = digit * Math.Pow(10, this.learnedScalings[0][i]) / 10;
-                    double magnitude = digit / 10 * Math.Sqrt(N + 1) / scaledValue;
+                    // Calculate the influence of the i-th digit on the output
+                    double influenceOnOutput = dOutput[j][i] / (influence / 10) * Math.Sqrt(N);
 
-                    // Compute the gradient with respect to the learned scaling
-                    double gradientMagnitude = dOutput[j][i + 1] / (magnitude * scaledValue); // Chain rule
-                    double gradientScaling = gradientMagnitude * Math.Log(10) * scaledNumber / 10;
+                    // Gradient of the output with respect to the scaling factor
+                    double scalingFactor = Math.Pow(10, this.learnedScalings[0][i]) / 10;
+                    double scalingFactorGradient = -influenceOnOutput / (scalingFactor * scalingFactor);
 
-                    dLearnedScalings[0][i] += gradientScaling;
+                    // Adjust the gradient by the steepness of the sigmoid and the derivative of the digit influence calculation
+                    dLearnedScalings[0][i] += scalingFactorGradient * Math.Log(10) * decimalNumber * Math.Pow(10, i) * influenceGradient;
                 }
             }
 
             return new BackwardResultBuilder()
-                .AddInputGradient(dInputMatrix)
+                .AddInputGradient(dInputMatrix) // Since this is not needed, it's left empty
                 .AddInputGradient(dLearnedScalings)
                 .Build();
+        }
+
+        private double CalculateDigitInfluence(double decimalNumber, int digitPosition)
+        {
+            // Shift the decimal number to focus on the current digit
+            double shiftedNumber = (decimalNumber * Math.Pow(10, digitPosition)) % 10;
+
+            // Use a sigmoid function to calculate the influence
+            // The sigmoid is centered around the middle of the digit (i.e., 5)
+            // Adjust the steepness of the sigmoid as needed
+            double steepness = 10; // Example value, adjust based on desired smoothness
+            return 1 / (1 + Math.Exp(-steepness * (shiftedNumber - 5)));
         }
     }
 }
