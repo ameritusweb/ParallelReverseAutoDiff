ae1d439a77d3cd1ed6d99d67c32ee358f240c813
Scott Seto <scott.seto@outlook.com>
2023-12-31 20:33:28 -0500

    Update network.

--- examples/gat/ParallelReverseAutoDiff.GatExample/OpticalCharacterRecognition/GraphAttentionNetwork/GraphAttentionNetwork.cs
+++ examples/gat/ParallelReverseAutoDiff.GatExample/OpticalCharacterRecognition/GraphAttentionNetwork/GraphAttentionNetwork.cs
@@ -60,7 +60,7 @@ namespace ParallelReverseAutoDiff.GatExample.OpticalCharacterRecognition.GraphAt
             for (int i = 0; i < this.NumLayers; ++i)
             {
                 var nestedLayerBuilder = new ModelLayerBuilder(this)
-                    .AddModelElementGroup("AdjacencyMatrix", new[] { numNodes, numNodes }, InitializationType.HeAdjacency)
+                    .AddModelElementGroup("AdjacencyMatrix", new[] { numLayers, numNodes, numNodes }, InitializationType.HeAdjacency)
                     .AddModelElementGroup("AttentionWeights", new[] { 1, numNestedOutputFeatures * 2 }, InitializationType.Xavier);
                 var nestedLayer = nestedLayerBuilder.Build();
                 this.nestedLayers.Add(nestedLayer);
@@ -295,79 +295,64 @@ namespace ParallelReverseAutoDiff.GatExample.OpticalCharacterRecognition.GraphAt
         /// <returns>A task.</returns>
         private async Task InitializeComputationGraph()
         {
-            var weightMatrix = this.embeddingLayer.WeightMatrix("Embeddings");
-            var gradientMatrix = this.embeddingLayer.GradientMatrix("Embeddings");
-            var outputDeepMatrix = this.outputLayer.WeightDeepMatrix("DM");
-            var outputGradientDeepMatrix = this.outputLayer.GradientDeepMatrix("DM");
-            var outputBias = this.outputLayer.WeightMatrix("KO");
-            var outputGradientBias = this.outputLayer.GradientMatrix("KO");
-
+            List<Matrix> linearWeights = new List<Matrix>();
+            List<Matrix> transformationBias = new List<Matrix>();
             List<Matrix> keys = new List<Matrix>();
-            List<Matrix> keysBias = new List<Matrix>();
             List<Matrix> values = new List<Matrix>();
-            List<Matrix> valuesBias = new List<Matrix>();
             List<Matrix> queries = new List<Matrix>();
-            List<Matrix> queriesBias = new List<Matrix>();
-            List<Matrix> reduce = new List<Matrix>();
-            List<Matrix> reduceBias = new List<Matrix>();
-            List<DeepMatrix> fully = new List<DeepMatrix>();
-            List<DeepMatrix> fullyBias = new List<DeepMatrix>();
-            List<DeepMatrix> fully2 = new List<DeepMatrix>();
-            List<DeepMatrix> fully2Bias = new List<DeepMatrix>();
+            List<DeepMatrix> adjacency = new List<DeepMatrix>();
+            List<Matrix> attentionWeights = new List<Matrix>();
+            List<Matrix> fully = new List<Matrix>();
+            List<Matrix> fullyBias = new List<Matrix>();
+            List<Matrix> fully2 = new List<Matrix>();
+            List<Matrix> fully2Bias = new List<Matrix>();
             List<Matrix> g = new List<Matrix>();
-            List<Matrix> cs = new List<Matrix>();
-            List<DeepMatrix> beta = new List<DeepMatrix>();
+            List<Matrix> beta = new List<Matrix>();
             for (int i = 0; i < this.NumLayers; ++i)
             {
+                linearWeights.Add(this.inputLayers[i].WeightMatrix("LinearWeights"));
+                transformationBias.Add(this.inputLayers[i].WeightMatrix("TransformationBias"));
                 keys.Add(this.inputLayers[i].WeightMatrix("Keys"));
-                keysBias.Add(this.inputLayers[i].WeightMatrix("KB"));
                 values.Add(this.inputLayers[i].WeightMatrix("Values"));
-                valuesBias.Add(this.inputLayers[i].WeightMatrix("VB"));
                 queries.Add(this.nestedLayers[i].WeightMatrix("Queries"));
-                queriesBias.Add(this.nestedLayers[i].WeightMatrix("QB"));
-                reduce.Add(this.outputLayers[i].WeightMatrix("R"));
-                reduceBias.Add(this.outputLayers[i].WeightMatrix("RB"));
-                fully.Add(this.outputLayers[i].WeightDeepMatrix("FW"));
-                fullyBias.Add(this.outputLayers[i].WeightDeepMatrix("FB"));
-                fully2.Add(this.outputLayers[i].WeightDeepMatrix("F2W"));
-                fully2Bias.Add(this.outputLayers[i].WeightDeepMatrix("F2B"));
-                beta.Add(this.outputLayers[i].WeightDeepMatrix("Beta"));
+                adjacency.Add(this.nestedLayers[i].WeightDeepMatrix("AdjacencyMatrix"));
+                attentionWeights.Add(this.nestedLayers[i].WeightMatrix("AttentionWeights"));
+                fully.Add(this.outputLayers[i].WeightMatrix("FW"));
+                fullyBias.Add(this.outputLayers[i].WeightMatrix("FB"));
+                fully2.Add(this.outputLayers[i].WeightMatrix("F2W"));
+                fully2Bias.Add(this.outputLayers[i].WeightMatrix("F2B"));
+                beta.Add(this.outputLayers[i].WeightMatrix("Beta"));
                 g.Add(this.outputLayers[i].WeightMatrix("G"));
-                cs.Add(this.outputLayers[i].WeightMatrix("CS"));
             }
 
+            List<Matrix> linearWeightsGradient = new List<Matrix>();
+            List<Matrix> transformationBiasGradient = new List<Matrix>();
             List<Matrix> keysGradient = new List<Matrix>();
-            List<Matrix> keysBiasGradient = new List<Matrix>();
             List<Matrix> valuesGradient = new List<Matrix>();
-            List<Matrix> valuesBiasGradient = new List<Matrix>();
             List<Matrix> queriesGradient = new List<Matrix>();
-            List<Matrix> queriesBiasGradient = new List<Matrix>();
-            List<Matrix> reduceGradient = new List<Matrix>();
-            List<Matrix> reduceBiasGradient = new List<Matrix>();
-            List<DeepMatrix> fullyGradient = new List<DeepMatrix>();
-            List<DeepMatrix> fullyBiasGradient = new List<DeepMatrix>();
-            List<DeepMatrix> fully2Gradient = new List<DeepMatrix>();
-            List<DeepMatrix> fully2BiasGradient = new List<DeepMatrix>();
+            List<DeepMatrix> adjacencyGradient = new List<DeepMatrix>();
+            List<Matrix> attentionWeightsGradient = new List<Matrix>();
+            List<Matrix> fullyGradient = new List<Matrix>();
+            List<Matrix> fullyBiasGradient = new List<Matrix>();
+            List<Matrix> fully2Gradient = new List<Matrix>();
+            List<Matrix> fully2BiasGradient = new List<Matrix>();
             List<Matrix> gGradient = new List<Matrix>();
-            List<Matrix> csGradient = new List<Matrix>();
-            List<DeepMatrix> betaGradient = new List<DeepMatrix>();
+            List<Matrix> betaGradient = new List<Matrix>();
             for (int i = 0; i < this.NumLayers; ++i)
             {
+                linearWeightsGradient.Add(this.inputLayers[i].GradientMatrix("LinearWeights"));
+                transformationBiasGradient.Add(this.inputLayers[i].GradientMatrix("TransformationBias"));
                 keysGradient.Add(this.inputLayers[i].GradientMatrix("Keys"));
-                keysBiasGradient.Add(this.inputLayers[i].GradientMatrix("KB"));
                 valuesGradient.Add(this.inputLayers[i].GradientMatrix("Values"));
-                valuesBiasGradient.Add(this.inputLayers[i].GradientMatrix("VB"));
                 queriesGradient.Add(this.nestedLayers[i].GradientMatrix("Queries"));
-                queriesBiasGradient.Add(this.nestedLayers[i].GradientMatrix("QB"));
-                reduceGradient.Add(this.outputLayers[i].GradientMatrix("R"));
-                reduceBiasGradient.Add(this.outputLayers[i].GradientMatrix("RB"));
-                fullyGradient.Add(this.outputLayers[i].GradientDeepMatrix("FW"));
-                fullyBiasGradient.Add(this.outputLayers[i].GradientDeepMatrix("FB"));
-                fully2Gradient.Add(this.outputLayers[i].GradientDeepMatrix("F2W"));
-                fully2BiasGradient.Add(this.outputLayers[i].GradientDeepMatrix("F2B"));
+                adjacencyGradient.Add(this.nestedLayers[i].GradientDeepMatrix("AdjacencyMatrix"));
+                attentionWeightsGradient.Add(this.nestedLayers[i].GradientMatrix("AttentionWeights"));
+                fullyGradient.Add(this.outputLayers[i].GradientMatrix("FW"));
+                fullyBiasGradient.Add(this.outputLayers[i].GradientMatrix("FB"));
+                fully2Gradient.Add(this.outputLayers[i].GradientMatrix("F2W"));
+                fully2BiasGradient.Add(this.outputLayers[i].GradientMatrix("F2B"));
+                betaGradient.Add(this.outputLayers[i].GradientMatrix("Beta"));
                 gGradient.Add(this.outputLayers[i].GradientMatrix("G"));
-                csGradient.Add(this.outputLayers[i].GradientMatrix("CS"));
-                betaGradient.Add(this.outputLayers[i].GradientDeepMatrix("Beta"));
             }
 
             string json = EmbeddedResource.ReadAllJson(NAMESPACE, ARCHITECTURE);
@@ -377,28 +362,21 @@ namespace ParallelReverseAutoDiff.GatExample.OpticalCharacterRecognition.GraphAt
                 .AddIntermediate("Output", _ => this.Output)
                 .AddIntermediate("Input", _ => this.Input)
                 .AddScalar("Divisor", x => 1d / Math.Pow(this.NumFeatures, 2))
-                .AddWeight("DM", _ => outputDeepMatrix).AddGradient("DDM", _ => outputGradientDeepMatrix)
-                .AddBias("KO", _ => outputBias).AddGradient("DKO", _ => outputGradientBias)
-                .AddWeight("Embeddings", _ => weightMatrix).AddGradient("DEmbeddings", _ => gradientMatrix)
+                .AddWeight("LinearWeights", x => linearWeights[x.Layer]).AddGradient("DLinearWeights", x => linearWeightsGradient[x.Layer])
+                .AddWeight("TransformationBias", x => transformationBias[x.Layer]).AddGradient("DTransformationBias", x => transformationBiasGradient[x.Layer])
                 .AddWeight("Keys", x => keys[x.Layer]).AddGradient("DKeys", x => keysGradient[x.Layer])
-                .AddBias("KB", x => keysBias[x.Layer]).AddGradient("DKB", x => keysBiasGradient[x.Layer])
                 .AddWeight("Values", x => values[x.Layer]).AddGradient("DValues", x => valuesGradient[x.Layer])
-                .AddBias("VB", x => valuesBias[x.Layer]).AddGradient("DVB", x => valuesBiasGradient[x.Layer])
                 .AddWeight("Queries", x => queries[x.Layer]).AddGradient("DQueries", x => queriesGradient[x.Layer])
-                .AddBias("QB", x => queriesBias[x.Layer]).AddGradient("DQB", x => queriesBiasGradient[x.Layer])
-                .AddWeight("R", x => reduce[x.Layer]).AddGradient("DR", x => reduceGradient[x.Layer])
-                .AddBias("RB", x => reduceBias[x.Layer]).AddGradient("DRB", x => reduceBiasGradient[x.Layer])
-                .AddWeight("FW", x => fully[x.Layer][x.NestedLayer]).AddGradient("DFW", x => fullyGradient[x.Layer][x.NestedLayer])
-                .AddWeight("FB", x => fullyBias[x.Layer][x.NestedLayer]).AddGradient("DFB", x => fullyBiasGradient[x.Layer][x.NestedLayer])
-                .AddWeight("F2W", x => fully2[x.Layer][x.NestedLayer]).AddGradient("DF2W", x => fully2Gradient[x.Layer][x.NestedLayer])
-                .AddWeight("F2B", x => fully2Bias[x.Layer][x.NestedLayer]).AddGradient("DF2B", x => fully2BiasGradient[x.Layer][x.NestedLayer])
-                .AddWeight("Beta", x => beta[x.Layer][x.NestedLayer]).AddGradient("DBeta", x => betaGradient[x.Layer][x.NestedLayer])
+                .AddWeight("FW", x => fully[x.Layer]).AddGradient("DFW", x => fullyGradient[x.Layer])
+                .AddWeight("FB", x => fullyBias[x.Layer]).AddGradient("DFB", x => fullyBiasGradient[x.Layer])
+                .AddWeight("F2W", x => fully2[x.Layer]).AddGradient("DF2W", x => fully2Gradient[x.Layer])
+                .AddWeight("F2B", x => fully2Bias[x.Layer]).AddGradient("DF2B", x => fully2BiasGradient[x.Layer])
+                .AddWeight("Beta", x => beta[x.Layer]).AddGradient("DBeta", x => betaGradient[x.Layer])
                 .AddBias("G", x => g[x.Layer]).AddGradient("DG", x => gGradient[x.Layer])
-                .AddBias("CS", x => cs[x.Layer]).AddGradient("DCS", x => csGradient[x.Layer])
+                .AddWeight("AdjacencyMatrix", x => adjacency[x.Layer][x.NestedLayer]).AddGradient("DAdjacencyMatrix", x => adjacencyGradient[x.Layer][x.NestedLayer])
+                .AddWeight("AttentionWeights", x => attentionWeights[x.Layer]).AddGradient("DAttentionWeights", x => attentionWeightsGradient[x.Layer])
                 .AddOperationFinder("nodeFeatures", x => x.Layer == 0 ? this.computationGraph["nodeFeatures_concatenate_0_0"] : this.computationGraph[$"output_act_0_{x.Layer - 1}"])
                 .AddOperationFinder("attention_scores_swiglu", x => x.NestedLayer == 0 ? this.computationGraph[$"attention_scores_0_{x.Layer}"] : this.computationGraph[$"swiglu_act_0_{x.Layer}_{x.NestedLayer - 1}"])
-                .AddOperationFinder("output_act_array", x => this.computationGraph.ToOperationArray("output_act", new LayerInfo(0, 0), new LayerInfo(0, this.NumLayers - 1)))
-                .AddOperationFinder("swiglu_act_array", x => this.computationGraph.ToOperationArray("swiglu_act", new LayerInfo(0, x.Layer, 0), new LayerInfo(0, x.Layer, this.NumQueries - 1)))
                 .ConstructFromArchitecture(jsonArchitecture, this.NumLayers, this.NumLayers);
 
             IOperationBase? backwardStartOperation = null;
